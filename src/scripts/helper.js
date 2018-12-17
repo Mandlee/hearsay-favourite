303c0522e3a0800682a849be889d16eddcb887cb
Bence Mándli <bence.mandli@attrecto.com>
2018-12-17 18:10:10 +0100

    Inital commit

---
+++ src/scripts/helper.js
@@ -0,0 +1,104 @@
+// helper
+//https://gist.github.com/nobitagit/74c507a96a73cbcace6c824bc9c2fecf
+/**
+ * Epochs
+ * @type {*[]}
+ */
+const epochs = [
+    ['year', 31536000],
+    ['month', 2592000],
+    ['day', 86400],
+    ['hour', 3600],
+    ['minute', 60],
+    ['second', 1]
+];
+
+/**
+ * Get duration
+ * @param timeAgoInSeconds {Number} - Time ago in seconds
+ * @returns {{interval: number, epoch: *}}
+ */
+const getDuration = (timeAgoInSeconds) => {
+    for (let [name, seconds] of epochs) {
+        const interval = Math.floor(timeAgoInSeconds / seconds);
+
+        if (interval >= 1) {
+            return {
+                interval: interval,
+                epoch: name
+            };
+        }
+    }
+};
+
+/**
+ * Time ago helper function
+ * @param date {Number} - UNIX timestamp
+ * @returns {string}
+ */
+export const timeAgo = (date) => {
+    const timeAgoInSeconds = Math.floor((new Date() - new Date(date)) / 1000);
+    const {interval, epoch} = getDuration(timeAgoInSeconds);
+    const suffix = interval === 1 ? '' : 's';
+
+    return `${interval} ${epoch}${suffix} ago`;
+};
+
+/**
+ * Transform call history to be easier to handle
+ * @param calls {Array} - Array of call history list
+ * @returns {Array}
+ */
+export const transformCalls = (calls) => {
+    if (!calls) {
+        return [];
+    }
+
+    let result = [];
+    let sortedResult;
+
+    for (let i = 0, length = calls.length; i < length; i++) {
+        const call = calls[i];
+
+        //Phone numbers it not seem to me unique. Example: Wallace phone number
+        const previousCallIndex = result.findIndex(elem => {
+            return elem.lastName === call.lastName && elem.firstName === call.firstName;
+        });
+
+
+        if (previousCallIndex !== -1) {
+            result[previousCallIndex].callsList.push({
+                phoneNumber: call.phoneNumber,
+                called: new Date(call.called * 1000)
+            });
+        } else {
+            result.push({
+                firstName: call.firstName,
+                lastName: call.lastName,
+                callsList: [{
+                    phoneNumber: call.phoneNumber,
+                    called: new Date(call.called * 1000)
+                }]
+            });
+        }
+    }
+
+    //
+    for (let i = 0, length = result.length; i < length; i++) {
+        const {callsList} = result[i];
+        result[i].callsList = callsList.sort((a, b) => {
+            return b.called - a.called;
+        });
+    }
+
+    //
+    sortedResult = result.sort((a, b) => {
+        if (a.callsList.length === b.callsList.length) {
+            return b.callsList[0].called - a.callsList[0].called;
+        }
+        return b.callsList.length - a.callsList.length;
+    });
+
+    console.log(sortedResult);
+    return sortedResult;
+};
---
+++ src/scripts/index.js
@@ -0,0 +1,26 @@
+import '../styles/index.scss';
+
+import callHistory from './callHistory.json';
+import {timeAgo, transformCalls} from "./helper";
+
+/**
+ * Render Call history
+ * @param element - Render into this element
+ * @param calls {Array} - Call history array
+ */
+const renderCalls = (element, calls) => {
+    try {
+        const result = transformCalls(calls).map(call =>
+            `<div class="call-list-item">
+                <div class="call-name">${call.firstName} <span class="last-name">${call.lastName}</span></div>
+                <div class="call-time">${timeAgo(call.callsList[0].called)} <div class="arrow-right"></div></div> 
+            </div>`
+        );
+        element.innerHTML = result.join('');
+    }
+    catch (e) {
+        console.error(e);
+    }
+};
+
+renderCalls(document.getElementById('content'), callHistory);
